03bad06b1bea5af8c242be4b45c7331744b73b7e
Kaioken1010 <jayesh113.jk@gmail.com>
2026-05-27 05:45:51 +0530

    Modify database initialization script for single user mode

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -4,7 +4,10 @@ GO
 --DROP AND RECREATE DATABASE 'DataWareHouse'--
 IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWareHouse') 
 BEGIN
-  ALTER DATABASE DataWareHouse
+  ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP Warehouse;
+  END
+GO
 
 
   --CREATING DATABASE 'DataWareHouse'--
